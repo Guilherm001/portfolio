c8566fc266850ced868988c35fc317e6b1ab4e0b
Guilherme <guilherm.rodrigues@outlook.com>
2025-06-26 17:30:34 -0300

    praticamente finalizado ja

--- src/app/components/Topo/index.jsx
+++ src/app/components/Topo/index.jsx
@@ -1,18 +1,37 @@
-import styles from './styles.module.css';
-
+import { useState } from "react";
+import styles from "./styles.module.css";
 
 export default function Topo() {
-    return (
-        <header className={styles.Topo}>
-        <h1>{'<Guilherme/>'}</h1>
-        <nav>
-            <ul className={styles.menu}>
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/">Sobre mim</a></li>
-            <li><a href="/">Habilidades</a></li>
-            <li><a href="/">Produtos</a></li>
-            </ul>
-        </nav>
-        </header>
-    );
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className={styles.Topo}>
+      <h1>{"<Guilherme/>"}</h1>
+      <nav>
+        <div
+          className={styles.hamburguer}
+          onClick={() => setOpen((prev) => !prev)}
+          aria-label="Abrir menu"
+        >
+          <span />
+          <span />
+          <span />
+        </div>
+        <ul className={`${styles.menu} ${open ? styles.open : ""}`}>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/">Sobre mim</a>
+          </li>
+          <li>
+            <a href="/">Habilidades</a>
+          </li>
+          <li>
+            <a href="/">Produtos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
