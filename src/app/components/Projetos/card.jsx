8fc44e7144f10f8bfacdb6c717ff985e27a1f03c
Guilherme <guilherm.rodrigues@outlook.com>
2025-06-26 13:50:00 -0300

    adicioando mais iffo

--- src/app/components/Projetos/card.jsx
+++ src/app/components/Projetos/card.jsx
@@ -2,22 +2,22 @@ import Style from "./Style.module.css";
 
 export default function Card(props) {
   return (
-    <div>
+    <div className={Style.Card}>
       <div className={Style.Imagem}>
         <h1>imagem</h1>
       </div>
-      <div>
+      <div className={Style.Titulo}>
         <h2>{props.titulo}</h2>
       </div>
-      <div>
+      <div className={Style.Descricao}>
         <p>{props.descricao}</p>
       </div>
-      <div>
+      <div className={Style.Tecnologias}>
         <p>{props.tecnologia1}</p>
         <p>{props.tecnologia2}</p>
         <p>{props.tecnologia3}</p>
       </div>
-      <nav>
+      <nav className={Style.Links}>
         <a href="#">Demo</a>
         <a href="#">Código</a>
       </nav>
--- src/app/components/Projetos/index.jsx
+++ src/app/components/Projetos/index.jsx
@@ -3,24 +3,22 @@ import Style from "./Style.module.css";
 
 export default function Projetos() {
   return (
-    <section>
-      <div>
+    <section className={Style.Projetos}>
+      <div className={Style.containerTitulo}>
         <h1>Projetos em Destaque</h1>
       </div>
-      <div>
-        <div>
-          <Card
-            imagem="/projeto1.png"
-            titulo="Projeto 1"
-            descricao="Descrição do Projeto 1"
-            tecnologia1=""
-            tecnologia2=""
-            tecnologia3=""
-            tecnologia4=""
-            link1=""
-            link2=""
-          />
-        </div>
+      <div className={Style.containerCards}>
+        <Card
+          imagem="/projeto1.png"
+          titulo="Projeto 1"
+          descricao="Descrição do Projeto 1"
+          tecnologia1=" React"
+          tecnologia2=" JavaScript"
+          tecnologia3=" CSS"
+          tecnologia4=" Next.js"
+          link1=""
+          link2=""
+        />
       </div>
     </section>
   );
